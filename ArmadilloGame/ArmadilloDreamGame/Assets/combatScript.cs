c0a61b6210ad88db573883573983df7932453cae
Jonathan Ribarro <Jcribarro@gmail.com>
2017-08-01 14:46:47 -0400

    Fixing up a brew of coding

--- ArmadilloGame/ArmadilloDreamGame/Assets/combatScript.cs
+++ ArmadilloGame/ArmadilloDreamGame/Assets/combatScript.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class combatScript : MonoBehaviour {
+    enum enemyStates
+    {
+        IDLE,
+        ATTACKING,
+        MISSED,
+        RANDOMATTACK
+    }
     float armadilloSpeed = 4.0f;
     Vector3 direction;
     Vector3 centerIsland;
     public static float distanceFromCenter = 50.0f;
+    public Vector3 currentPlayerPos;// = 
+    Vector3 rotationDirection;
+    Vector3 rotateToPlayerPos;
+    Vector3 zeroVec;
+
+  
+    enemyStates bossStates;
 	// Use this for initialization
 	void Start ()
     {
-		centerIsland = new Vector3(255,15,255);
-	}
+        zeroVec = new Vector3(0, 0, 0);
+        currentPlayerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        centerIsland = new Vector3(255,15,255);
+        rotateToPlayerPos = GameObject.FindGameObjectWithTag("Player").transform.position - transform.position;
+        rotationDirection = rotateToPlayerPos;
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -43,19 +61,62 @@ public class combatScript : MonoBehaviour {
 
     void armadilloAttackPlayer()
     {
-        this.transform.position += transform.forward * armadilloSpeed * Time.deltaTime;
-     //   yield return new WaitForSeconds(3);
+        while (transform.position != currentPlayerPos) 
+        {
+            this.transform.position += transform.forward * armadilloSpeed * Time.deltaTime;
+            rotationDirection = new Vector3(0,0,0);
+        }
+        //   yield return new WaitForSeconds(3);
+        //Vector3 tempCurrentPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        direction.x = 0;
+        direction.y = 0;
+        direction.z = 0;
+
 
     }
 
     void armadilloRotate()
     {
         direction.y = 0;
-        
-        Vector3 rotationDirection = GameObject.FindGameObjectWithTag("Player").transform.position - transform.position;
+      //  rotationDirection = 
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(rotationDirection), 0.5f);
+
 
     }
 
+
+    void armadilloStates(enemyStates states)
+    {
+        switch(states)
+        {
+            case enemyStates.IDLE:
+                {
+                    armadilloSpeed = 0;
+                }
+                break;
+            case enemyStates.ATTACKING:
+                {
+                    armadilloSpeed = 5;
+                    armadilloRotate();
+                    armadilloAttackPlayer();
+                }
+                break;
+            case enemyStates.MISSED:
+                {
+                    armadilloSpeed = 0;
+                }
+                break;
+            case enemyStates.RANDOMATTACK:
+                {
+
+                }
+                break;
+            default:
+                armadilloRotate();
+                break;
+          //  default: enemyStates.IDLE
+
+        }
+    }
 
 }
